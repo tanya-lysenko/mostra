b4950072d4c1168098d597d8b15351c1a75ad72d
Tanya <82pripri82@gmail.com>
2021-04-09 20:57:00 +0300

    Images

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
-$(function() {
-    // Пустые ссылки
-    $('a[href="#"]').click(function (e) {
+document.addEventListener('click', function (e) {
+    const condition1 = e.target.href === '#';
+    const condition2 = e.target.closest(`[href="#"]`);
+
+    if (condition1 || condition2) {
         e.preventDefault();
-    });
-
-    // Масштабируемые картинки
-    $(".img-scalable").each(function () {
-        var box = $(this).find(".img-box"),
-            k = parseInt(box.css("max-width"), 10) / parseInt(box.css("max-height"), 10)
-        ;
-        $(document).ready(function () {
-            box.height(box.width() / k);
-        });
-        $(window).resize(function () {
-            box.height(box.width() / k);
-        });
-    });
-
-    // Мобильное меню
-    $("a.bars").click(function () {
-        $("#header nav").toggleClass("visible");
-    });
+    }
 });
+
+const menuBars = document.querySelector('#header .bars');
+const menuList = document.querySelector('#header nav');
+
+menuBars.addEventListener('click', function () {
+    menuList.classList.toggle('visible');
+});
+
+// $(function() {
+//     // Масштабируемые картинки
+//     $(".img-scalable").each(function () {
+//         var box = $(this).find(".img-box"),
+//             k = parseInt(box.css("max-width"), 10) / parseInt(box.css("max-height"), 10)
+//         ;
+//         $(document).ready(function () {
+//             box.height(box.width() / k);
+//         });
+//         $(window).resize(function () {
+//             box.height(box.width() / k);
+//         });
+//     });
+//
+// });
